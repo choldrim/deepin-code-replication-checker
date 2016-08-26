817dc6e9d536c89a59f1001ac0de8e4251ef8d6e
Choldrim <choldrim@foxmail.com>
2016-08-26 09:05:36 +0800

    Revert "Return failed code after some problems checked out"
    This reverts commit 40ee5282ca6935de3754a00181cf06c675ec051f.

--- main.py
+++ main.py
@@ -44,12 +44,6 @@ def work():
     results[gh.get_name()] = check(base, gh)
 
     gen_report(results)
-    # for return code
-    for (item, problems) in results.items():
-        if len(problems) > 0:
-            return 1
-
-    return 0
 
 
 def check(base, target, with_private=False):
@@ -158,4 +152,4 @@ if __name__ == '__main__':
     if CACHE_MODE:
         print('cache mode(for debug), load data from cache file')
 
-    quit(work())
+    work()
